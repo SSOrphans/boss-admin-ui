1f31541c5a3b341a8c4d6770341fd0f0da65fd18
Derrian Harris <dsh0018au@gmail.com>
2021-05-11 17:14:50 -0500

    SSOR-175 Added reactstrap and implemented basic table

--- src/app/loans/Loans.js
+++ src/app/loans/Loans.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { InputGroup, Table } from "react-bootstrap";
+import { InputGroup, Table, Input, Container } from "reactstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchAllLoans } from "./loanService";
 
@@ -14,9 +14,46 @@ export default function Loans() {
 
 	console.log(loans);
 	return (
-		<div>
-			<InputGroup></InputGroup>
-			<Table></Table>
-		</div>
+		<Container class='container-fluid mt-2'>
+			<form>
+				<div class='form-group form-inline'>
+					<input
+						class='form-control'
+						type='text'
+						placeholder='Enter loan number..'
+					/>
+				</div>
+			</form>
+			<Table striped bordered responsive='lg'>
+				<tr>
+					<th>Loan Number</th>
+					<th>User Id</th>
+					<th>Branch Id</th>
+					<th>Type</th>
+					<th>Amount Due</th>
+					<th>Interest Rate</th>
+					<th>Due By</th>
+					<th>Taken At</th>
+					<th>Total</th>
+				</tr>
+				<tbody>
+					{loans.content.map((loan) => {
+						return (
+							<tr>
+								<td>{loan.loanNumber}</td>
+								<td>{loan.userId}</td>
+								<td>{loan.branchId}</td>
+								<td>{loan.loanType}</td>
+								<td>{loan.amountDue}</td>
+								<td>{loan.interestRate}</td>
+								<td>{loan.dueBy}</td>
+								<td>{loan.takenAt}</td>
+								<td>{loan.amount}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</Table>
+		</Container>
 	);
 }
