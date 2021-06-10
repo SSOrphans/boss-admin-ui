8071e509b5f2373333b9a48f85377157ef329dea
Bermond Jon Batistiana <bermond.batistiana@smoothstack.com>
2021-06-09 18:53:25 -0700

    SSOR-251 Added Homepage Login

--- src/app/features/home/components/HomeComponent.js
+++ src/app/features/home/components/HomeComponent.js
@@ -1,9 +1,18 @@
 import React from "react";
+import {LoginComponent} from "./LoginComponent";
+import {useSelector} from "react-redux";
 
 export const HomeComponent = () => {
+  
+  const currentState = useSelector((state) => state.login);
+  
   return (
-    <div >
-    
+    <div>
+      {currentState.loginState ?
+        ""
+        :
+        <LoginComponent/>
+      }
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { Provider } from 'react-redux';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-	<React.StrictMode>
+	// <React.StrictMode>
 		<Provider store={store}>
 			<App />
-		</Provider>
-	</React.StrictMode>,
+		</Provider>,
+	//</React.StrictMode>,
 	document.getElementById("root")
 );
 
